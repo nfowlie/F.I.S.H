4188940ee7c7b19ba4e40277c15e687d204737fb
Neil <neil.fowlie@gmail.com>
2018-03-06 21:47:03 -0500

    Current Work

---
+++ main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const url = require('url');
+const shell = require('electron').shell;
+const ipc = require('electron').ipcMain;
+
+require('electron-reload')(__dirname);
+
+var win;
+
+function createWindow() {
+    // Create new window
+    win = new BrowserWindow({width: 1000, height: 800});
+    // Load HTML into window
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                { type: 'separator' },
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit();
+                    }
+                },
+            ]
+        },
+        {
+            label: 'Info'
+        }
+    ]);
+
+    Menu.setApplicationMenu(menu);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+});
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const BrowserWindow = electron.remote.BrowserWindow;
+const ipc = electron.ipcRenderer;
+
+let win;
+
+const fishBtn = document.getElementById("fish");
+fishBtn.addEventListener("click", function(event){
+    
+    // win = new BrowserWindow({width: 1000, height: 800});
+    // const viewPath = path.join("file://", __dirname, "views/fish.html");
+    // win.loadURL(url.format({
+    //     pathname: path.join(__dirname, 'views/fish.html'),
+    //     protocol: 'file:',
+    //     slashes: true
+    // }));
+});
